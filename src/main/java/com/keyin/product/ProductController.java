e691e6143b6a4ff3d1c5c3f537750bc47c08542f
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2025-08-05 09:51:20 -0230

    Initial commit

---
+++ src/main/java/com/keyin/product/ProductController.java
@@ -0,0 +1,19 @@
+package com.keyin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public List<String> getProducts() {
+        return List.of("Product 1", "Product 2", "Product 3");
+    }
+
+
+}
